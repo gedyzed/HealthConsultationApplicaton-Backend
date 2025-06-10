2e4b298340c791c8b381d41ca09877f5a703fe74
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-06-11 01:43:22 +0300

    Update patientController.php

--- app/Http/Controllers/patientController.php
+++ app/Http/Controllers/patientController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class patientController extends Controller
 {
