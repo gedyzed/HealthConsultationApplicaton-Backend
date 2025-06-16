935718f5b5423d1f504745451b46ddc8561bb617
Gedion Zeyede <gedion.zeyede-ug@aau.edu.et>
2025-06-16 08:54:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::post('/doctor/editProfile/{id}', [docterController::class, 'editProfile']
 // ->middleware("auth:api");
 Route::get('/patientList/{id}',[docterController::class,"getPatientList"]);
 //  ->middleware("auth:api");
-Route::get('/doctors/{id}/unverified-doctor', [docterController::class, 'getUserWithUnverifiedDoctor']);
+Route::get('/doctors/unverified-doctor', [docterController::class, 'getUserWithUnverifiedDoctor']);
 
 //appointments
 Route::post("/setAppointment",[appointmentController::class, "setAppointment"]);
